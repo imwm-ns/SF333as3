8164d3c82011fe1444eb45504920512b643e1bff
imwm-ns <narathip_aot@hotmail.com>
2023-10-10 15:11:16 +0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 
+//      Narathip Jaroensuk 6410742412
+//      Sittipak Srisawas 6410742032
+//      Wa
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Narathip Jaroensuk on 10/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
